2dfb92ca792178e1faf85bffc650eefe6bd6edad
Eric Zhang <ekzhang1@gmail.com>
2020-05-19 20:09:52 -0500

    Change develop branch to setwithfriends-dev SDK

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import "firebase/auth";
 import "firebase/analytics";
 
 const config = {
-  apiKey: "AIzaSyCeKQ4rauZ_fq1rEIPJ8m5XfppwjtmTZBY",
-  authDomain: "setwithfriends.com",
-  databaseURL: "https://setwithfriends.firebaseio.com",
-  projectId: "setwithfriends",
-  storageBucket: "setwithfriends.appspot.com",
-  messagingSenderId: "970544876139",
-  appId: "1:970544876139:web:06295fe4079007f76abf2e",
-  measurementId: "G-QDX193SN7R"
+  apiKey: "AIzaSyB6jICg__HEdtZRcSoIoeUMau41jvKNwvU",
+  authDomain: "setwithfriends-dev.firebaseapp.com",
+  databaseURL: "https://setwithfriends-dev.firebaseio.com",
+  projectId: "setwithfriends-dev",
+  storageBucket: "setwithfriends-dev.appspot.com",
+  messagingSenderId: "369319422608",
+  appId: "1:369319422608:web:b9038b38a1bd598048c615",
+  measurementId: "G-GN0204W8F7",
 };
 
 firebase.initializeApp(config);
