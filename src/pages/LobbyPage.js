d00fffa9cfbafd3ccdf786af8aff0418595876a3
Eric Zhang <ekzhang1@gmail.com>
2020-05-20 14:39:30 -0500

    Add lobby design mockup

--- src/pages/LobbyPage.js
+++ src/pages/LobbyPage.js
@@ -1,299 +1,298 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import generate from "project-name-generator";
-import parseColor from "parse-color";
 import { Link as RouterLink } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { Redirect } from "react-router";
-import Card from "@material-ui/core/Card";
+import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
 import Link from "@material-ui/core/Link";
-import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogActions from "@material-ui/core/DialogActions";
-import FormControl from "@material-ui/core/FormControl";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Divider from "@material-ui/core/Divider";
+import HourglassEmptyRounded from "@material-ui/icons/HourglassEmptyRounded";
+import PlayArrowRounded from "@material-ui/icons/PlayArrowRounded";
+import DoneRounded from "@material-ui/icons/DoneRounded";
+import Face from "@material-ui/icons/Face";
+import SportsEsports from "@material-ui/icons/SportsEsports";
 
-import PromptDialog from "../components/PromptDialog";
-import firebase from "../firebase";
-import { computeState } from "../util";
+import autoscroll from "../utils/autoscroll";
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    padding: 40,
-    height: "100%",
-    textAlign: "center"
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    "--table-height": "400px", // responsive variable
+    [theme.breakpoints.up("sm")]: {
+      "--table-height": "480px",
+    },
+    [theme.breakpoints.up("md")]: {
+      "--table-height": "calc(100vh - 140px)",
+    },
   },
-  menu: {
-    padding: 12,
+  gamesTable: {
+    height: "var(--table-height)",
+    whiteSpace: "nowrap",
+    "& td, & th": {
+      paddingTop: 6,
+      paddingBottom: 6,
+      paddingLeft: 12,
+      paddingRight: 12,
+    },
+    "& svg": {
+      display: "block",
+    },
+    "& tr:hover": {
+      background: theme.palette.action.hover,
+      cursor: "pointer",
+    },
+  },
+  lobbyTabs: {
+    minHeight: 32,
+    "& .MuiTab-root": {
+      minHeight: 32,
+      textTransform: "none",
+      fontWeight: 400,
+    },
+  },
+  gameCounters: {
+    [theme.breakpoints.up("sm")]: {
+      position: "absolute",
+      bottom: 8,
+    },
+  },
+  actionButtons: {
+    [theme.breakpoints.up("sm")]: {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      "& button": {
+        margin: "18px 0",
+      },
+    },
+    [theme.breakpoints.down("xs")]: {
+      "& button": {
+        marginBottom: theme.spacing(1),
+      },
+    },
+  },
+  chatColumn: {
     display: "flex",
     flexDirection: "column",
-    "& button": {
-      margin: 12,
-      marginTop: 6,
-      marginBottom: 6
+    maxHeight: "calc(var(--table-height) + 16px)",
+    [theme.breakpoints.up("md")]: {
+      marginTop: 36,
     },
-    "& button:first-child": {
-      marginTop: 12,
-      marginBottom: 12
-    },
-    "& button:last-child": {
-      marginBottom: 12
-    }
   },
-  warningBtn: {
-    color: theme.palette.warning.dark
-  },
-  optionsForm: {
+  chatPanel: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
     display: "flex",
-    marginBottom: 12,
-    "& > div": {
-      flexGrow: 1
-    }
-  }
+    flexDirection: "column",
+  },
+  buttonColumn: {
+    position: "relative",
+    maxHeight: "calc(var(--table-height) + 16px)",
+    [theme.breakpoints.up("sm")]: {
+      marginTop: 36,
+    },
+  },
 }));
 
 function LobbyPage({ user }) {
   const classes = useStyles();
   const [redirect, setRedirect] = useState(null);
-  const [play, setPlay] = useState(false);
-  const [join, setJoin] = useState(false);
-  const [spectate, setSpectate] = useState(false);
-  const [options, setOptions] = useState(false);
-  const [games, setGames] = useState({});
-  const [userName, setUserName] = useState("Anonymous");
-  const [userColor, setUserColor] = useState("#888888");
+  const chatEl = useRef();
 
   useEffect(() => {
-    const gameList = user.games ? Object.values(user.games) : [];
-    const gamesRef = firebase.database().ref("games/");
-    const cleanup = [];
-    for (const gameId of gameList) {
-      const gameRef = gamesRef.child(gameId);
-      const update = snapshot => {
-        const game = snapshot.val();
-        if (game.meta.status === "done") {
-          const { scores } = computeState(game);
-          setGames(games => ({
-            ...games,
-            [gameId]: {
-              winner: scores[0][0],
-              score: scores.filter(([u, s]) => u === user.id)[0][1]
-            }
-          }));
-        }
-      };
-      gameRef.on("value", update);
-      cleanup.push(() => gameRef.off("value", update));
-    }
-    return () => {
-      for (const func of cleanup) func();
-    };
-  }, [user]);
-
-  const stats = Object.values(games).reduce(
-    ([p, w, s], g) => [p + 1, w + (g.winner === user.id), s + g.score],
-    [0, 0, 0]
-  );
+    return autoscroll(chatEl.current);
+  }, []);
 
   if (redirect) return <Redirect push to={redirect} />;
-
-  function playButton() {
-    setPlay(true);
-  }
 
   function newRoom() {
     setRedirect("/room/" + generate().dashed);
   }
 
-  function joinRoom() {
-    setJoin(true);
-  }
-
-  function handleJoin(gameId) {
-    setJoin(false);
-    if (gameId) {
-      setRedirect(`/room/${gameId}`);
-    }
-  }
-
-  function spectateGame() {
-    setSpectate(true);
-  }
-
-  function handleSpectate(gameId) {
-    setSpectate(false);
-    if (gameId) {
-      setRedirect(`/game/${gameId}`);
-    }
-  }
-
-  function optionsButton() {
-    setUserName(user.name);
-    setUserColor(user.color);
-    setOptions(true);
-  }
-
-  function handleReset() {
-    setOptions(false);
-    firebase.auth().currentUser.delete();
-  }
-
-  function handleSave() {
-    setOptions(false);
-    if (userName && userColor) {
-      firebase
-        .database()
-        .ref(`users/${user.id}`)
-        .set({
-          name: userName,
-          color: userColor
-        });
-    }
-  }
-
   return (
-    <Container className={classes.container}>
-      <Dialog open={play} onClose={() => setPlay(false)}>
-        <DialogTitle>Play Set</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Welcome! Set with Friends is a web app that lets you play Set in
-            real time with others online. To begin, you can either{" "}
-            <b>create a new room</b> and share the link with friends, or{" "}
-            <b>join an existing game</b> by entering the ID.
-          </DialogContentText>
-          <DialogContentText>
-            <Link component={RouterLink} to="/help">
-              View help page
-            </Link>
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={() => {
-              setPlay(false);
-              newRoom();
-            }}
-            color="primary"
-          >
-            New Room
-          </Button>
-          <Button
-            onClick={() => {
-              setPlay(false);
-              joinRoom();
-            }}
-            color="primary"
-          >
-            Join Room
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <PromptDialog
-        open={join}
-        onClose={handleJoin}
-        title="Join Room"
-        message="To join a game with others, please enter the room ID below. Alternatively, you can also enter a direct link to the game in the address bar."
-        label="Room ID"
-      />
-      <PromptDialog
-        open={spectate}
-        onClose={handleSpectate}
-        title="Spectate"
-        message="To spectate a game, please enter the room ID below. You will be redirected to the game page, where you can watch the action live."
-        label="Room ID"
-      />
-      <Dialog open={options} onClose={() => setOptions(false)}>
-        <DialogTitle>Options</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            You can configure your nickname and in-game display color below. You
-            can also reset your data, including user statistics.
-          </DialogContentText>
-          <form
-            className={classes.optionsForm}
-            onSubmit={e => e.preventDefault()}
-          >
-            <FormControl>
-              <InputLabel htmlFor="name-input">Name</InputLabel>
-              <Input
-                id="name-input"
-                value={userName}
-                onChange={e => setUserName(e.target.value)}
-                inputProps={{ maxLength: 40 }}
-              />
-            </FormControl>
-            <FormControl>
-              <InputLabel htmlFor="color-input">Color</InputLabel>
-              <Input
-                id="color-input"
-                type="color"
-                value={parseColor(userColor).hex}
-                onChange={e => setUserColor(e.target.value)}
-              />
-            </FormControl>
-          </form>
-        </DialogContent>
-        <DialogActions>
-          <Button className={classes.warningBtn} onClick={handleReset}>
-            Reset Data
-          </Button>
-          <Button variant="contained" color="primary" onClick={handleSave}>
-            Save
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <Typography variant="h3" component="h2" gutterBottom>
-        Set with Friends
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={4}>
-          {/* empty */}
-        </Grid>
-        <Grid item xs={4}>
-          <Card elevation={2} className={classes.menu}>
-            <Button onClick={playButton} variant="contained" color="primary">
-              Play
-            </Button>
-            <Button onClick={newRoom} variant="contained">
-              New Room
-            </Button>
-            <Button onClick={joinRoom} variant="contained">
-              Join Room by ID
-            </Button>
-            <Button onClick={spectateGame} variant="contained">
-              Spectate
-            </Button>
-            <Button onClick={optionsButton} variant="contained">
-              Options
-            </Button>
-          </Card>
-        </Grid>
-        <Grid item xs={3}>
-          <Card className={classes.menu}>
-            <Typography variant="h6" gutterBottom>
-              Statistics
-            </Typography>
-            <Typography variant="body1">Games played: {stats[0]}</Typography>
-            <Typography variant="body1">Games won: {stats[1]}</Typography>
-            <Typography variant="body1">Total sets: {stats[2]}</Typography>
-            <Typography variant="body1">
-              Avg. sets per game:{" "}
-              {stats[0] ? (stats[2] / stats[0]).toFixed(2) : "N/A"}
-            </Typography>
-          </Card>
-        </Grid>
-        <Grid item xs={1}>
-          {/* empty */}
-        </Grid>
+    <Container>
+      <Grid container spacing={2} className={classes.mainGrid}>
+        <Box clone order={{ xs: 3, md: 1 }} className={classes.chatColumn}>
+          <Grid item xs={12} sm={12} md={3}>
+            <section
+              className={classes.chatPanel}
+              style={{
+                flexShrink: 0,
+                maxHeight: "40%",
+              }}
+            >
+              <Typography variant="overline">Online Users (5)</Typography>
+              <List
+                dense
+                style={{ paddingTop: 0, overflowY: "auto", flexGrow: 1 }}
+              >
+                <ListItem button>
+                  <ListItemIcon>
+                    <Face />
+                  </ListItemIcon>
+                  <ListItemText>Eric Zhang</ListItemText>
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <SportsEsports />
+                  </ListItemIcon>
+                  <ListItemText>Cynthia Du</ListItemText>
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <SportsEsports />
+                  </ListItemIcon>
+                  <ListItemText>Anonymous Polar Bear</ListItemText>
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <Face />
+                  </ListItemIcon>
+                  <ListItemText>Anonymous Skunk</ListItemText>
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <Face />
+                  </ListItemIcon>
+                  <ListItemText>Anonymous Ant</ListItemText>
+                </ListItem>
+              </List>
+            </section>
+            <Divider />
+            <section
+              className={classes.chatPanel}
+              style={{ flexGrow: 1, overflowY: "hidden" }}
+            >
+              <Typography variant="overline">Lobby Chat</Typography>
+              <div style={{ overflowY: "auto", flexGrow: 1 }} ref={chatEl}>
+                {[...Array(50)].map((_, i) => (
+                  <div key={i}>
+                    <b>Eric Zhang</b>: Hello world {i}!
+                  </div>
+                ))}
+              </div>
+              <form onSubmit={(e) => e.preventDefault() || alert("chat!")}>
+                <input
+                  style={{ width: "100%" }}
+                  placeholder="Press [Enter] to chat"
+                />
+              </form>
+            </section>
+            <Divider />
+          </Grid>
+        </Box>
+        <Box clone order={{ xs: 1, md: 2 }}>
+          <Grid item xs={12} sm={8} md={6}>
+            <Tabs
+              className={classes.lobbyTabs}
+              indicatorColor="secondary"
+              textColor="secondary"
+              variant="fullWidth"
+              value={0}
+            >
+              <Tab label="Lobby"></Tab>
+              <Tab label="Your games"></Tab>
+            </Tabs>
+            <TableContainer component={Paper} className={classes.gamesTable}>
+              <Table size="small" stickyHeader>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Host</TableCell>
+                    <TableCell>Size</TableCell>
+                    <TableCell>Status</TableCell>
+                    <TableCell>Time</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {[...Array(30)].map((_, i) =>
+                    i % 2 ? (
+                      <TableRow key={i} onClick={() => setRedirect("/game")}>
+                        <TableCell>Anonymous Polar Bear</TableCell>
+                        <TableCell>3</TableCell>
+                        <TableCell>
+                          <PlayArrowRounded fontSize="small" />
+                        </TableCell>
+                        <TableCell>{i + 2} minutes ago</TableCell>
+                      </TableRow>
+                    ) : i % 3 ? (
+                      <TableRow key={i} onClick={() => setRedirect("/game")}>
+                        <TableCell>Anonymous Ant</TableCell>
+                        <TableCell>2</TableCell>
+                        <TableCell>
+                          <DoneRounded fontSize="small" />
+                        </TableCell>
+                        <TableCell>{i + 2} minutes ago</TableCell>
+                      </TableRow>
+                    ) : (
+                      <TableRow key={i} onClick={() => setRedirect("/game")}>
+                        <TableCell>Anonymous Dragonfly</TableCell>
+                        <TableCell>2</TableCell>
+                        <TableCell>
+                          <HourglassEmptyRounded fontSize="small" />
+                        </TableCell>
+                        <TableCell>{i + 2} minutes ago</TableCell>
+                      </TableRow>
+                    )
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Box>
+        <Box clone order={{ xs: 2, md: 3 }} className={classes.buttonColumn}>
+          <Grid item xs={12} sm={4} md={3}>
+            <div className={classes.actionButtons}>
+              <Button
+                variant="contained"
+                fullWidth
+                color="primary"
+                onClick={newRoom}
+              >
+                Create a Game
+              </Button>
+              <Button variant="contained" fullWidth>
+                New Private Game
+              </Button>
+            </div>
+            <div className={classes.gameCounters}>
+              <Typography variant="body2" gutterBottom>
+                <b>37,000</b> games played
+              </Typography>
+            </div>
+          </Grid>
+        </Box>
       </Grid>
+      <Typography variant="body1" align="center" style={{ margin: 16 }}>
+        <Link component={RouterLink} to="/help">
+          Help
+        </Link>{" "}
+        •{" "}
+        <Link component={RouterLink} to="/about">
+          About
+        </Link>{" "}
+        •{" "}
+        <Link component={RouterLink} to="/contact">
+          Contact
+        </Link>
+      </Typography>
     </Container>
   );
 }
