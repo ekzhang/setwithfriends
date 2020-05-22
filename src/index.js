447d74ee62878c2c66f77edd2f59119e06468a93
Cynthia <cynthiad@mit.edu>
2020-05-21 21:46:14 -0400

    Keep track of url for each connection

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { BrowserRouter as Router } from "react-router-dom";
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Router>
+    <App />{" "}
+  </Router>,
+  rootElement
+);
