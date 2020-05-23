078107c9e773e9bbb5547af28a9aa73084a7e37f
Eric Zhang <ekzhang1@gmail.com>
2020-05-23 11:24:41 -0500

    Fix major performance bug in useMoment

--- src/hooks/useMoment.js
+++ src/hooks/useMoment.js
@@ -4,7 +4,7 @@ import moment from "moment";
 function useMoment() {
   const [time, setTime] = useState(moment());
   useEffect(() => {
-    const id = setInterval(() => setTime(moment(), 1000));
+    const id = setInterval(() => setTime(moment()), 1000);
     return () => clearInterval(id);
   }, []);
 
