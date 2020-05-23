4bfdaea3d003cdd3b1f05c3a89630864c7f9b914
Cynthia <cynthiad@mit.edu>
2020-05-23 19:08:21 -0400

    Estimate server time instead of using local reported time

--- src/hooks/useMoment.js
+++ src/hooks/useMoment.js
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
 import moment from "moment";
+import useFirebaseRef from "./useFirebaseRef";
 
 function useMoment() {
-  const [time, setTime] = useState(moment());
+  const [time, setTime] = useState(moment()); //firebase server time
+  const [offSet] = useFirebaseRef(".info/serverTimeOffset");
+
   useEffect(() => {
-    const id = setInterval(() => setTime(moment()), 1000);
-    return () => clearInterval(id);
-  }, []);
+    const id = setInterval(() => {
+      setTime(moment().add(offSet, "ms"));
+    }, 1000);
+    return () => {
+      clearInterval(id); //Any setIntervals() that saw stale offSet is cleared
+    };
+  }, [offSet]);
 
   return time;
 }
