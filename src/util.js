66165799232d2a8dc069925bdef8aad819a23fd6
Eric Zhang <ekzhang1@gmail.com>
2020-06-04 22:37:35 -0500

    Hotfix for broken user colors

--- src/util.js
+++ src/util.js
@@ -1,6 +1,38 @@
 import animals from "./utils/animals.json";
 import moment from "moment";
-import * as colors from "@material-ui/core/colors";
+import red from "@material-ui/core/colors/red";
+import pink from "@material-ui/core/colors/pink";
+import purple from "@material-ui/core/colors/purple";
+import deepPurple from "@material-ui/core/colors/deepPurple";
+import indigo from "@material-ui/core/colors/indigo";
+import blue from "@material-ui/core/colors/blue";
+import lightBlue from "@material-ui/core/colors/lightBlue";
+import cyan from "@material-ui/core/colors/cyan";
+import teal from "@material-ui/core/colors/teal";
+import green from "@material-ui/core/colors/green";
+import lightGreen from "@material-ui/core/colors/lightGreen";
+import lime from "@material-ui/core/colors/lime";
+import amber from "@material-ui/core/colors/amber";
+import orange from "@material-ui/core/colors/orange";
+import deepOrange from "@material-ui/core/colors/deepOrange";
+
+export const colors = {
+  red,
+  pink,
+  purple,
+  deepPurple,
+  indigo,
+  blue,
+  lightBlue,
+  cyan,
+  teal,
+  green,
+  lightGreen,
+  lime,
+  amber,
+  orange,
+  deepOrange,
+};
 
 export function generateCards() {
   const deck = [];
