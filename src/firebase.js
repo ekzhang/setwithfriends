025346a64d8397bd2f6ec62e2f23cf59e82a12a2
Eric Zhang <ekzhang1@gmail.com>
2020-06-04 21:20:15 -0500

    Prepare for production release

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import "firebase/analytics";
 import "firebase/functions";
 
 const config = {
-  apiKey: "AIzaSyB6jICg__HEdtZRcSoIoeUMau41jvKNwvU",
-  authDomain: "setwithfriends-dev.firebaseapp.com",
-  databaseURL: "https://setwithfriends-dev.firebaseio.com",
-  projectId: "setwithfriends-dev",
-  storageBucket: "setwithfriends-dev.appspot.com",
-  messagingSenderId: "369319422608",
-  appId: "1:369319422608:web:b9038b38a1bd598048c615",
-  measurementId: "G-GN0204W8F7",
+  apiKey: "AIzaSyCeKQ4rauZ_fq1rEIPJ8m5XfppwjtmTZBY",
+  authDomain: "setwithfriends.com",
+  databaseURL: "https://setwithfriends.firebaseio.com",
+  projectId: "setwithfriends",
+  storageBucket: "setwithfriends.appspot.com",
+  messagingSenderId: "970544876139",
+  appId: "1:970544876139:web:06295fe4079007f76abf2e",
+  measurementId: "G-QDX193SN7R",
 };
 
 firebase.initializeApp(config);
