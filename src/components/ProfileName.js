1cd9d1e4e406ef811c8ed1cda7a417a1ae8c364a
Cynthia <cynthiad@mit.edu>
2020-06-24 14:15:23 -0400

    Mockup for username reset button

--- src/components/ProfileName.js
+++ src/components/ProfileName.js
@@ -1,13 +1,44 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 
 import Typography from "@material-ui/core/Typography";
 import { useTheme } from "@material-ui/core/styles";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { makeStyles } from "@material-ui/core/styles";
 
 import ElapsedTime from "./ElapsedTime";
 import User from "./User";
 
+const useStyles = makeStyles((theme) => ({
+  vertIcon: {
+    marginLeft: "auto",
+    cursor: "pointer",
+    "&:hover": {
+      fill: "pink",
+    },
+  },
+}));
+
 function ProfileName({ userId }) {
   const theme = useTheme();
+  const classes = useStyles();
+
+  const [showVertIcon, setShowVertIcon] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClickVertIcon = (event) => {
+    setAnchorEl(event.currentTarget);
+    setMenuOpen(true);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setMenuOpen(false);
+    setShowVertIcon(false);
+  };
 
   return (
     <User
@@ -17,13 +48,37 @@ function ProfileName({ userId }) {
           user.connections && Object.keys(user.connections).length > 0;
         return (
           <section>
-            <Typography
-              variant="h4"
-              gutterBottom
-              style={{ overflowWrap: "anywhere" }}
+            <div
+              style={{ display: "flex", flexDirection: "row" }}
+              onMouseEnter={() => setShowVertIcon(true)}
+              onMouseLeave={() => setShowVertIcon(false)}
             >
-              {userEl}
-            </Typography>
+              <Typography
+                variant="h4"
+                gutterBottom
+                style={{ overflowWrap: "anywhere" }}
+              >
+                {userEl}
+              </Typography>
+              <MoreVertIcon
+                aria-controls="admin-menu"
+                color="inherit"
+                className={classes.vertIcon}
+                style={{
+                  opacity: `${showVertIcon ? 1 : 0}`,
+                }}
+                onClick={(e) => handleClickVertIcon(e)}
+              />
+              <Menu
+                id="admin-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl) && menuOpen}
+                onClose={handleClose}
+              >
+                <MenuItem>Reset username</MenuItem>
+              </Menu>
+            </div>
+
             <Typography variant="body2" gutterBottom>
               Last seen:{" "}
               <span
