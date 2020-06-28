8f8dd2c482d5c8b6b9b7127a7f3e9b4a5b639586
Eric Zhang <ekzhang1@gmail.com>
2020-06-28 00:35:34 -0500

    Fix bug with incorrect forwardRef

--- src/components/InternalLink.js
+++ src/components/InternalLink.js
@@ -3,8 +3,8 @@ import React, { forwardRef } from "react";
 import Link from "@material-ui/core/Link";
 import { Link as RouterLink } from "react-router-dom";
 
-function InternalLink(props) {
-  return <Link component={RouterLink} {...props} />;
+function InternalLink(props, ref) {
+  return <Link ref={ref} component={RouterLink} {...props} />;
 }
 
 export default forwardRef(InternalLink);
