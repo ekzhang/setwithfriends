32b2947aa4c8075ecb900c29662c95ad72bf2646
Eric Zhang <ekzhang1@gmail.com>
2020-12-19 15:49:14 -0600

    Add donation buttons

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -13,41 +13,42 @@ function AboutPage() {
       </Typography>
       <Paper style={{ padding: "1rem", maxWidth: 720, margin: "12px auto" }}>
         <Typography variant="body1" gutterBottom>
-          Set with Friends was developed by{" "}
+          Set with Friends was started by{" "}
           <Link href="https://github.com/ekzhang">Eric Zhang</Link> and{" "}
-          <Link href="https://github.com/cynthiakedu">Cynthia Du</Link> as a
-          side project in early 2020. We love the game, and our goal was to
-          bridge the distance between friends by creating the simplest interface
-          for playing Set online.
+          <Link href="https://github.com/cynthiakedu">Cynthia Du</Link> in early
+          2020. We love the game, and our goal was to bridge the distance
+          between friends by creating the simplest, free interface for playing
+          Set online.
         </Typography>
         <Typography variant="body1" gutterBottom>
-          The source code and technical details are available on{" "}
+          The code powering this site is completely open source and available on{" "}
           <Link href="https://github.com/ekzhang/setwithfriends">GitHub</Link>.
-          We are open to contributors from all backgrounds, whether you're a
-          seasoned programmer or just want to learn more about web development.
-          Please see the repository for detailed instructions.
+          We are happy to provide mentorship for contributors from all
+          backgrounds, whether you're a seasoned programmer or just want to
+          learn more about web development.
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          This site would not be possible without many people's help: the{" "}
+          <Link href="https://github.com/ekzhang/setwithfriends/graphs/contributors">
+            volunteer developers
+          </Link>{" "}
+          who contribute code to add features and fix bugs, the{" "}
+          <Link href="https://www.patreon.com/setwithfriends">patrons</Link> who
+          help keep the site running, and many others who reported bugs and
+          provided feedback.
         </Typography>
         <Typography variant="body1" gutterBottom>
           <strong>
-            If you have comments or suggestions, the best way to reach us is by{" "}
+            If you have suggestions, the best way to reach us is by{" "}
             <Link href="https://github.com/ekzhang/setwithfriends/issues">
               filing an issue
             </Link>
             .
           </strong>{" "}
-          You can also contact the authors by email at{" "}
+          You can also contact the lead developers by email at{" "}
           <Link href="mailto:ekzhang1@gmail.com">ekzhang1@gmail.com</Link> and{" "}
           <Link href="mailto:cynthiakedu@gmail.com">cynthiakedu@gmail.com</Link>
           .
-        </Typography>
-        <hr style={{ margin: "1em 0" }} />
-        <Typography variant="body1" gutterBottom>
-          Thanks to our code contributors{" "}
-          <Link href="https://github.com/tarehart">Tyler Arehart</Link>,{" "}
-          <Link href="https://github.com/stevenhao">Steven Hao</Link>,{" "}
-          <Link href="https://github.com/eparadise">Eliza Paradise</Link>, and{" "}
-          <Link href="https://github.com/wenley">Wenley Tong</Link>, as well as
-          the many others who reported bugs and provided feedback.
         </Typography>
       </Paper>
       <Typography variant="body1" align="center" style={{ margin: 12 }}>
