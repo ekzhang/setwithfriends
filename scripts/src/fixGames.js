9b6ad41771fd37e589e87ccbd0f89001391f1b1e
Eric Zhang <ekzhang1@gmail.com>
2024-12-22 15:14:09 -0600

    Fix up scripts with batched iterator, add toggleAdmin

--- scripts/src/fixGames.js
+++ scripts/src/fixGames.js
@@ -1,10 +1,9 @@
 import assert from "assert";
-import admin from "firebase-admin";
+import { getDatabase } from "firebase-admin/database";
 
 /** Fix games that have `endedAt === 0` due to a site migration bug in v3.0.0. */
 export async function fixGames() {
-  const badGames = await admin
-    .database()
+  const badGames = await getDatabase()
     .ref("games")
     .orderByChild("endedAt")
     .equalTo(0)
@@ -14,8 +13,7 @@ export async function fixGames() {
     console.log(`Fixing game ${gameId}...`);
     console.log({ [gameId]: game });
     assert.strictEqual(game.status, "done");
-    const events = await admin
-      .database()
+    const events = await getDatabase()
       .ref(`gameData/${gameId}/events`)
       .once("value");
     let lastTime = 0;
@@ -23,7 +21,7 @@ export async function fixGames() {
       lastTime = event.val().time;
     });
     console.log(`Setting endedAt = ${lastTime}...`);
-    await admin.database().ref(`games/${gameId}/endedAt`).set(lastTime);
+    await getDatabase().ref(`games/${gameId}/endedAt`).set(lastTime);
     console.log("Done.\n");
   }
 
