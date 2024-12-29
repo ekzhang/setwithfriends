ekzhang/setwithfriends

commit f4b77d94bcff70373b6fbf38cf0fabe313c74567

Implement Set Junior mode (#177)
Brought up today in community meeting as a popular game mode. Set Junior involves a 27 card deck.

For security / database integrity reasons at scale, I'm taking a different approach than the one in #148, filtering out the cards from the deck post hoc.

Set Junior has hints enabled. Looking through the code, there's no reason why other game modes can't have hints enabled either. I can flip this switch in the future if there's demand for it, will probably want to write a migration though to check integrity of previous setchain/ultraset games that had hints on in the database but are currently interpreted as "off" due to hints not being supported.

src/util.js
@@ -58,6 +58,13 @@ export const modes = {
     description: "Find 3 cards that form a Set.",
     setType: "Set",
   },
+  setjr: {
+    name: "Junior",
+    color: "green",
+    description:
+      "A simplified version that only uses cards with solid shading.",
+    setType: "Set",
+  },
   setchain: {
     name: "Set-Chain",
     color: "teal",
@@ -169,6 +176,7 @@ export function findSet(deck, gameMode = "normal", old) {
       const c = conjugateCard(deck[i], deck[j]);
       if (
         gameMode === "normal" ||
+        gameMode === "setjr" ||
         (gameMode === "setchain" && old.length === 0)
       ) {
         if (deckSet.has(c)) {
@@ -290,11 +298,26 @@ function processEventUltra(internalGameState, event) {
   internalGameState.boardSize = boardSize;
 }
 
+/**
+ * Initialize the deck to its starting cards based on the game mode.
+ *
+ * It starts with a shuffled 81-card deck according to database state. Usually
+ * this would be a no-op, but for Set Junior, we need to remove some subset of
+ * the cards before the game starts.
+ */
+export function initializeDeck(deck, gameMode) {
+  if (gameMode === "setjr") {
+    // Remove all cards except those with solid shading.
+    return deck.filter((card) => card[2] === "0");
+  }
+  return deck;
+}
+
 export function computeState(gameData, gameMode = "normal") {
   const scores = {}; // scores of all users
   const used = {}; // set of cards that have been taken
   const history = []; // list of valid events in time order
-  const current = gameData.deck.slice(); // remaining cards in the game
+  const current = initializeDeck(gameData.deck, gameMode); // remaining cards in the game
   const internalGameState = {
     used,
     current,
@@ -311,7 +334,8 @@ export function computeState(gameData, gameMode = "normal") {
       })
       .map(([, e]) => e);
     for (const event of events) {
-      if (gameMode === "normal") processEventNormal(internalGameState, event);
+      if (gameMode === "normal" || gameMode === "setjr")
+        processEventNormal(internalGameState, event);
       if (gameMode === "setchain") processEventChain(internalGameState, event);
       if (gameMode === "ultraset") processEventUltra(internalGameState, event);
     }
@@ -335,6 +359,6 @@ export function hasHint(game) {
     game.users &&
     Object.keys(game.users).length === 1 &&
     game.access === "private" &&
-    (game.mode || "normal") === "normal"
+    (game.mode === "normal" || game.mode === "setjr")
   );
 }

functions/src/game.ts
@@ -9,7 +9,7 @@ interface GameEvent {
   c4?: string;
 }
 
-export type GameMode = "normal" | "setchain" | "ultraset";
+export type GameMode = "normal" | "setjr" | "setchain" | "ultraset";
 
 /** Generates a random 81-card deck using a Fisher-Yates shuffle. */
 export function generateDeck() {
@@ -71,6 +71,7 @@ export function findSet(deck: string[], gameMode: GameMode, old?: string[]) {
       const c = conjugateCard(deck[i], deck[j]);
       if (
         gameMode === "normal" ||
+        gameMode === "setjr" ||
         (gameMode === "setchain" && old!.length === 0)
       ) {
         if (deckSet.has(c)) {
@@ -89,6 +90,21 @@ export function findSet(deck: string[], gameMode: GameMode, old?: string[]) {
     }
   }
   return null;
+}
+
+/**
+ * Initialize the deck to its starting cards based on the game mode.
+ *
+ * It starts with a shuffled 81-card deck according to database state. Usually
+ * this would be a no-op, but for Set Junior, we need to remove some subset of
+ * the cards before the game starts.
+ */
+function initializeDeck(deck: string[], gameMode: GameMode) {
+  if (gameMode === "setjr") {
+    // Remove all cards except those with solid shading.
+    return deck.filter((card) => card[2] === "0");
+  }
+  return deck;
 }
 
 /** Check if cards are valid (all distinct and exist in deck) */
@@ -162,13 +178,18 @@ export function replayEvents(
   // Array.sort() is guaranteed to be stable in Node.js, and the latest ES spec
   events.sort((e1, e2) => e1.time - e2.time);
 
-  const deck: Set<string> = new Set(gameData.child("deck").val());
+  const deck: Set<string> = new Set(
+    initializeDeck(gameData.child("deck").val(), gameMode),
+  );
   const history: GameEvent[] = [];
   const scores: Record<string, number> = {};
   let finalTime = 0;
   for (const event of events) {
     let eventValid = false;
-    if (gameMode === "normal" && replayEventNormal(deck, event))
+    if (
+      (gameMode === "normal" || gameMode === "setjr") &&
+      replayEventNormal(deck, event)
+    )
       eventValid = true;
     if (gameMode === "setchain" && replayEventChain(history, deck, event))
       eventValid = true;

functions/src/index.ts
@@ -97,7 +97,7 @@ export const finishGame = functions.https.onCall(async (data, context) => {
     snapshot.child("enableHint").val() &&
     snapshot.child("users").numChildren() === 1 &&
     snapshot.child("access").val() === "private" &&
-    gameMode === "normal"
+    (gameMode === "normal" || gameMode === "setjr")
   ) {
     return;
   }

scripts/src/calcStats.js
@@ -1,221 +1,0 @@
-// Note: This is deprecated.
-import { getDatabase } from "firebase-admin/database";
-
-const batchSize = 1000;
-
-/**
- * Recalculate statistics for all users based on their game history.
- *
- * Run this twice, before and after the v3.2.1 release, as well as any future
- * release that changes the statistics calculated for each user.
- *
- * Warning: This script is extremely CPU and memory-intensive. Past iterations
- * blew through the maximum Node.js heap allocation size, so I had to batch the
- * calculations to be efficient.
- */
-export async function calcStats() {
-  console.log("Loading users...");
-  const users = await getDatabase().ref("users").orderByKey().get();
-  const userIds = Object.keys(users.val());
-  console.log("Done loading users!");
-
-  for (let i = 0; i < userIds.length; i += batchSize) {
-    console.log(`Progress: ${i}/${userIds.length}`);
-    const startTime = Date.now();
-
-    await Promise.all(
-      userIds.slice(i, i + batchSize).map(async (userId) => {
-        const games = await getDatabase()
-          .ref(`userGames/${userId}`)
-          .once("value");
-
-        if (process.env.VERBOSE)
-          console.log(`[${userId}] Found ${games.numChildren()} games...`);
-        if (games.numChildren() === 0) {
-          return;
-        }
-
-        const stats = {};
-        for (const mode of ["normal", "setchain", "ultraset"]) {
-          stats[mode] = {};
-          for (const variant of ["solo", "multiplayer"]) {
-            stats[mode][variant] = {};
-          }
-        }
-
-        await Promise.all(
-          Object.keys(games.val()).map(async (gameId) => {
-            const game = await getDatabase().ref(`games/${gameId}`).get();
-            if (game.child("status").val() !== "done") {
-              if (process.env.VERBOSE)
-                console.log(`[${userId}] Skipping ongoing game ${gameId}`);
-              return;
-            }
-            const gameData = await getDatabase()
-              .ref(`gameData/${gameId}`)
-              .get();
-
-            if (process.env.VERBOSE)
-              console.log(`[${userId}] Adding game ${gameId}`);
-
-            const gameMode = game.child("mode").val() || "normal";
-            const { finalTime, scores } = replayEvents(gameData, gameMode);
-
-            // Check if hints are enabled; and if so, ignore the game in statistics
-            if (
-              game.child("enableHint").val() &&
-              game.child("users").numChildren() === 1 &&
-              game.child("access").val() === "private" &&
-              gameMode === "normal"
-            ) {
-              return;
-            }
-
-            // Retrieve userIds of players in the game.
-            const players = [];
-            game.child("users").forEach(function (childSnap) {
-              if (childSnap.key !== null) {
-                players.push(childSnap.key);
-              }
-            });
-
-            if (!players.includes(userId)) {
-              // Database inconsistency between userGames and games/*/users
-              return;
-            }
-
-            // Add scores for players without a single set.
-            for (const player of players) {
-              scores[player] = scores[player] || 0;
-            }
-
-            // Differentiate between solo and multiplayer games.
-            const variant = players.length === 1 ? "solo" : "multiplayer";
-            const time = finalTime - game.child("startedAt").val();
-            const topScore = Math.max(...Object.values(scores));
-
-            // Update statistics
-            const obj = stats[gameMode][variant];
-            obj.finishedGames = (obj.finishedGames || 0) + 1;
-            obj.totalSets = (obj.totalSets || 0) + scores[userId];
-            if (scores[userId] === topScore) {
-              obj.wonGames = (obj.wonGames || 0) + 1;
-              obj.fastestTime = Math.min(obj.fastestTime || Infinity, time);
-            }
-            obj.totalTime = (obj.totalTime || 0) + time;
-          }),
-        );
-
-        if (process.env.VERBOSE)
-          console.log(`[${userId}] Finished: ${JSON.stringify(stats)}`);
-
-        const updates = {};
-        for (const mode of ["normal", "setchain", "ultraset"]) {
-          for (const variant of ["solo", "multiplayer"]) {
-            updates[`${mode}/${variant}`] = stats[mode][variant];
-          }
-        }
-        await getDatabase().ref(`userStats/${userId}`).update(updates);
-      }),
-    );
-
-    const elapsed = Date.now() - startTime;
-    console.log(`  -> took ${elapsed}ms`);
-  }
-}
-
-//
-// The functions below are from `functions/src/game.ts`, with types removed.
-//
-
-/** Check if cards are valid (all distinct and exist in deck) */
-function isValid(deck, cards) {
-  for (let i = 0; i < cards.length; i++) {
-    for (let j = i + 1; j < cards.length; j++) {
-      if (cards[i] === cards[j]) return false;
-    }
-    if (!deck.has(cards[i])) return false;
-  }
-  return true;
-}
-
-/** Delete cards from deck */
-function deleteCards(deck, cards) {
-  for (const c of cards) deck.delete(c);
-}
-
-/** Replay game event for normal mode */
-function replayEventNormal(deck, event) {
-  const cards = [event.c1, event.c2, event.c3];
-  if (!isValid(deck, cards)) return false;
-  deleteCards(deck, cards);
-  return true;
-}
-
-/** Replay game event for setchain mode */
-function replayEventChain(history, deck, event) {
-  const { c1, c2, c3 } = event;
-
-  // Check validity
-  let ok = c1 !== c2 && c2 !== c3 && c1 !== c3;
-  ok = ok && deck.has(c2) && deck.has(c3);
-  if (history.length) {
-    // One card (c1) should be taken from the previous set
-    const prevEvent = history[history.length - 1];
-    const prev = [prevEvent.c1, prevEvent.c2, prevEvent.c3];
-    ok = ok && prev.includes(c1);
-  }
-  if (!ok) return;
-
-  const cards = history.length === 0 ? [c1, c2, c3] : [c2, c3];
-  deleteCards(deck, cards);
-  return true;
-}
-
-/** Replay game event for ultraset mode */
-function replayEventUltra(deck, event) {
-  const cards = [event.c1, event.c2, event.c3, event.c4];
-  if (!isValid(deck, cards)) return false;
-  deleteCards(deck, cards);
-  return true;
-}
-
-/**
- * Compute remaining cards (arbitrary order) left in the deck after some
- * events, as well as the time of the final valid event.
- */
-export function replayEvents(gameData, gameMode) {
-  const events = [];
-  gameData.child("events").forEach((e) => {
-    events.push(e.val());
-  });
-  // Array.sort() is guaranteed to be stable in Node.js, and the latest ES spec
-  events.sort((e1, e2) => e1.time - e2.time);
-
-  const deck = new Set(gameData.child("deck").val());
-  const history = [];
-  const scores = {};
-  let finalTime = 0;
-  for (const event of events) {
-    let eventValid = false;
-    if (gameMode === "normal" && replayEventNormal(deck, event))
-      eventValid = true;
-    if (gameMode === "setchain" && replayEventChain(history, deck, event))
-      eventValid = true;
-    if (gameMode === "ultraset" && replayEventUltra(deck, event))
-      eventValid = true;
-    if (eventValid) {
-      history.push(event);
-      scores[event.user] = (scores[event.user] || 0) + 1;
-      finalTime = event.time;
-    }
-  }
-
-  let lastSet = [];
-  if (gameMode === "setchain" && history.length > 0) {
-    const lastEvent = history[history.length - 1];
-    lastSet = [lastEvent.c1, lastEvent.c2, lastEvent.c3];
-  }
-
-  return { lastSet, deck, finalTime, scores };
-}

scripts/src/index.js
@@ -2,20 +2,12 @@ import { cert, initializeApp } from "firebase-admin/app";
 import inquirer from "inquirer";
 import process from "node:process";
 
-import { calcStats } from "./calcStats.js";
 import { fixGames } from "./fixGames.js";
 import { sanitizeNames } from "./sanitizeNames.js";
 import { listAdmins, listPatrons, toggleAdmin } from "./users.js";
 
 // Add scripts as functions to this array
-const scripts = [
-  listAdmins,
-  listPatrons,
-  toggleAdmin,
-  sanitizeNames,
-  fixGames,
-  calcStats,
-];
+const scripts = [listAdmins, listPatrons, toggleAdmin, sanitizeNames, fixGames];
 
 initializeApp({
   credential: cert("./credential.json"),

src/components/ChatCards.js
@@ -43,7 +43,9 @@ function ChatCards({ item, gameMode, startedAt }) {
             id={item.user}
           />
         </div>
-        {(gameMode === "normal" || gameMode === "setchain") && (
+        {(gameMode === "normal" ||
+          gameMode === "setjr" ||
+          gameMode === "setchain") && (
           <div>
             <SetCard size="sm" value={item.c1} />
             <SetCard size="sm" value={item.c2} />

src/components/GameSettings.js
@@ -9,7 +9,12 @@ import firebase from "../firebase";
 import { hasHint, modes } from "../util";
 
 const useStyles = makeStyles(() => ({
-  settings: { display: "flex", flexDirection: "column", alignItems: "center" },
+  settings: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingInline: 4,
+  },
 }));
 
 const hintTip =
@@ -31,8 +36,13 @@ function GameSettings({ game, gameId, userId }) {
 
   return (
     <div className={classes.settings}>
-      <RadioGroup row value={gameMode} onChange={handleChangeMode}>
-        {["normal", "setchain", "ultraset"].map((mode) => (
+      <RadioGroup
+        row
+        sx={{ justifyContent: "center" }}
+        value={gameMode}
+        onChange={handleChangeMode}
+      >
+        {Object.keys(modes).map((mode) => (
           <Tooltip
             key={mode}
             arrow
@@ -41,22 +51,31 @@ function GameSettings({ game, gameId, userId }) {
           >
             <FormControlLabel
               value={mode}
-              control={<Radio />}
+              control={<Radio size="small" sx={{ width: 32, height: 32 }} />}
               disabled={userId !== game.host}
               label={modes[mode].name}
+              slotProps={{ typography: { variant: "body2" } }}
             />
           </Tooltip>
         ))}
       </RadioGroup>
-      {gameMode === "normal" && (
+      {["normal", "setjr"].includes(gameMode) && (
         <Tooltip arrow placement="left" title={hintTip}>
           <FormControlLabel
-            control={<Switch checked={hasHint(game)} onChange={toggleHint} />}
+            control={
+              <Switch
+                size="small"
+                checked={hasHint(game)}
+                onChange={toggleHint}
+              />
+            }
             label="Enable Hints"
             disabled={
               Object.keys(game.users || {}).length > 1 ||
               game.access !== "private"
             }
+            slotProps={{ typography: { variant: "body2" } }}
+            sx={{ my: 0.25 }}
           />
         </Tooltip>
       )}

src/pages/GamePage.js
@@ -214,7 +214,7 @@ function GamePage() {
     lastSet = [c1, c2, c3];
   }
   let answer = findSet(current.slice(0, boardSize), gameMode, lastSet);
-  if (gameMode === "normal" && hasHint(game) && answer) {
+  if (hasHint(game) && answer) {
     answer = answer.slice(0, numHints);
   } else {
     answer = null;
@@ -236,7 +236,7 @@ function GamePage() {
       if (selected.includes(card)) {
         return removeCard(selected, card);
       } else {
-        if (gameMode === "normal") {
+        if (gameMode === "normal" || gameMode === "setjr") {
           const vals = [...selected, card];
           if (vals.length === 3) {
             if (checkSet(...vals)) {
@@ -480,7 +480,7 @@ function GamePage() {
         >
           <GameSidebar game={game} scores={scores} leaderboard={leaderboard} />
           <Box mt={1}>
-            {gameMode === "normal" && hasHint(game) && (
+            {hasHint(game) && (
               <Button
                 size="large"
                 variant="outlined"

commit e551587a5c37ee618048f779a52b9e86fb5d2e2b

Fix a silly bug introduced in #177

src/util.js
@@ -310,7 +310,7 @@ export function initializeDeck(deck, gameMode) {
     // Remove all cards except those with solid shading.
     return deck.filter((card) => card[2] === "0");
   }
-  return deck;
+  return deck.slice();
 }
 
 export function computeState(gameData, gameMode = "normal") {