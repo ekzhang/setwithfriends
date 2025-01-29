fa501b10d04a0a1b890ae464f599ee41f031c4cc
Eric Zhang <ekzhang1@gmail.com>
2025-01-29 17:29:03 -0500

    Save hopefully ~$200/month (#183)
    * Save hopefully ~$200/month
    
    Turns out that this consumes a lot of bandwidth from RTDB, I'm seeing like 8 GB/day. Which is kind of insane :(
    
    I think the bandwidth is being counted even for rows that are never returned from the query due to being filtered out. Firebase is just really inefficient. So it's iterating through basically the entire `gameData` database field every time I run this function, ugh.
    
    Making some changes to reduce expenditure.
    
    * Actually save money by improving indexing
    
    Why doesn't Firebase have query iterators…

--- scripts/src/utils.js
+++ scripts/src/utils.js
@@ -6,23 +6,29 @@ import { getDatabase } from "firebase-admin/database";
  *
  * @param {string} path The path to the reference to iterate through.
  * @param {number} batchSize The number of children to fetch in each batch.
+ * @param {string} [start] The key to start at (inclusive).
+ * @param {string} [end] The key to end at (inclusive).
  * @returns {AsyncGenerator<[string, import("firebase-admin/database").DataSnapshot]>}
  */
-export async function* databaseIterator(path, batchSize = 1000) {
-  let lastKey = null;
+export async function* databaseIterator(
+  path,
+  batchSize = 1000,
+  start, // inclusive
+  end, // inclusive
+) {
+  let lastKey = undefined;
   while (true) {
-    const snap = lastKey
-      ? await getDatabase()
-          .ref(path)
-          .orderByKey()
-          .startAfter(lastKey)
-          .limitToFirst(batchSize)
-          .get()
-      : await getDatabase()
-          .ref(path)
-          .orderByKey()
-          .limitToFirst(batchSize)
-          .get();
+    let query = getDatabase().ref(path).orderByKey();
+    if (lastKey !== undefined) {
+      query = query.startAfter(lastKey);
+    } else if (start !== undefined) {
+      query = query.startAt(start);
+    }
+    if (end !== undefined) {
+      query = query.endAt(end);
+    }
+
+    const snap = await query.limitToFirst(batchSize).get();
     if (!snap.exists()) return;
 
     const childKeys = [];
